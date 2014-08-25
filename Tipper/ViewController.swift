7a59e9b12f9eea1ff074e35a5be82e4002f0b3be
Nag Varun Chunduru <nchunduru@hearsaycorp.com>
2014-08-24 20:47:24 -0700

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Nag Varun Chunduru on 8/24/14.
+//  Copyright (c) 2014 cnv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
